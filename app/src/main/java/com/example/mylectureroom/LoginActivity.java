fec94be2dadf54bd4796f53b4cdb274a5e35ad43
Lee Hyeong Min <myung4386@naver.com>
2019-11-27 20:35:32 +0900

    회원가입, 로그인 구현

---
+++ app/src/main/java/com/example/mylectureroom/LoginActivity.java
@@ -0,0 +1,218 @@
+package com.example.mylectureroom;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static String IP_ADDRESS = "155.230.52.54:9900";
+    private static String TAG = "phptest";
+
+    private static final String TAG_JSON="MYJSON";
+    private static final String TAG_ID = "ID";
+    private static final String TAG_PASSWORD = "PASSWORD";
+
+    private EditText mID;
+    private EditText mPassword;
+    private TextView mTextViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mID = (EditText)findViewById(R.id.editText_loginID);
+        mPassword = (EditText)findViewById(R.id.editText_loginPassword);
+        mTextViewResult = (TextView)findViewById(R.id.textView_login_result);
+        mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+        Button buttonInsert = (Button)findViewById(R.id.button_login_insert);
+        Button button_goto = (Button)findViewById(R.id.button_goto_main);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String id = mID.getText().toString();
+                String password = mPassword.getText().toString();
+
+                GetData task = new GetData();
+                task.execute("http://" + IP_ADDRESS + "/login.php", id, password);
+
+
+                mID.setText("");
+                mPassword.setText("");
+            }
+        });
+
+        button_goto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(LoginActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(TAG, "response - " + result);
+
+            System.out.println(result + "111111");
+            if (result == null){
+
+                mTextViewResult.setText(errorString);
+            }
+            else if (result.equals("fail")){
+                mTextViewResult.setText("ID or Password is wrong.");
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setMessage("ID or PW is wrong. please check again.");
+                builder.setTitle("Error")
+                        .setCancelable(false)
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int i){
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("Error");
+                alert.show();
+            }
+
+            else {
+                Intent intent = new Intent(getApplicationContext(), TempActivity.class);
+                startActivity(intent);
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String gID = (String)params[1];
+            String gPassword = (String)params[2];
+            String SHAPassword = SHAEncode(gPassword);
+            String serverURL = (String)params[0];
+            System.out.println(gID + "1111111");
+            System.out.println(SHAPassword + "2222222");
+            String postParameters = "ID=" + gID + "&PW=" + SHAPassword;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+
+        protected String SHAEncode(String password) {
+            try {
+
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(password.getBytes("UTF-8"));
+                StringBuffer hexString = new StringBuffer();
+
+                for (int i = 0; i < hash.length; i++) {
+                    String hex = Integer.toHexString(0xff & hash[i]);
+                    if (hex.length() == 1) hexString.append('0');
+                    hexString.append(hex);
+                }
+
+                return hexString.toString();
+
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/mylectureroom/MainActivity.java
+++ app/src/main/java/com/example/mylectureroom/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mylectureroom;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,50 +23,199 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import java.security.MessageDigest;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static String IP_ADDRESS = "155.230.52.54:9900";
     private static String TAG = "phptest";
 
-    private EditText mEditTextName;
-    private EditText mEditTextCountry;
+    private EditText mID;
+    private EditText mPassword;
+    private EditText mPassword2;
     private TextView mTextViewResult;
-
+    private static final String TAG_JSON = "MYJSON";
+    private static final String TAG_ID = "ID";
+    String mJsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEditTextName = (EditText)findViewById(R.id.editText_main_name);
-        mEditTextCountry = (EditText)findViewById(R.id.editText_main_country);
+        mID = (EditText)findViewById(R.id.editText_ID);
+        mPassword = (EditText)findViewById(R.id.editText_Password);
+        mPassword2 = (EditText)findViewById(R.id.editText_Password2);
         mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
-
         mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
 
 
+
         Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
+        Button button_goto = (Button)findViewById(R.id.button_goto_login);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String id = mEditTextName.getText().toString();
-                String password = mEditTextCountry.getText().toString();
-
-                InsertData task = new InsertData();
-                task.execute("http://" + IP_ADDRESS + "/insert.php", id,password);
+                String id = mID.getText().toString();
+                String password = mPassword.getText().toString();
+                String password2  = mPassword2.getText().toString();
 
 
-                mEditTextName.setText("");
-                mEditTextCountry.setText("");
+                GetData task = new GetData();
+                task.execute("http://" + IP_ADDRESS + "/dupcheck.php", id, password, password2);
 
+
+                mID.setText("");
+                mPassword.setText("");
+                mPassword2.setText("");
             }
         });
 
+        button_goto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    private class GetData extends AsyncTask<String, Void, String>{
 
+        ProgressDialog progressDialog;
+        String errorString = null;
+        String gID;
+        String gPassword;
+        String gPassword2;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            Log.d(TAG, "response - " + result);
+
+            if(result == null) {
+                mTextViewResult.setText(errorString);
+            }
+            else if (!result.equals("fail")){
+                mTextViewResult.setText("Duplicated ID. Please Enter Another ID.");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Duplicated ID");
+                builder.setTitle("Error")
+                        .setCancelable(false)
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int i){
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("Error");
+                alert.show();
+            }
+            else if (!gPassword.equals(gPassword2)){
+                mTextViewResult.setText("Passwords are not same. Please enter correctly.");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Passwords are not same");
+                builder.setTitle("Error")
+                        .setCancelable(false)
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int i){
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("Error");
+                alert.show();
+            }
+            else{
+
+                InsertData task2 = new InsertData();
+                task2.execute("http://" + IP_ADDRESS + "/insert.php", gID, gPassword);
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            gID = (String)params[1];
+            gPassword = (String)params[2];
+            gPassword2 = (String)params[3];
+            String serverURL = (String)params[0];
+            String postParameters = "ID=" + gID;
+
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
 
     class InsertData extends AsyncTask<String, Void, String>{
         ProgressDialog progressDialog;
@@ -92,9 +244,9 @@ public class MainActivity extends AppCompatActivity {
 
             String id = (String)params[1];
             String password = (String)params[2];
-
             String serverURL = (String)params[0];
-            String postParameters = "id=" + id + "&password=" + password;
+            String SHAPassword = SHAEncode(password);
+            String postParameters = "id=" + id + "&password=" + SHAPassword;
 
 
             try {
@@ -135,7 +287,7 @@ public class MainActivity extends AppCompatActivity {
 
                 while((line = bufferedReader.readLine()) != null){
                     sb.append(line);
-                }
+            }
 
 
                 bufferedReader.close();
@@ -152,7 +304,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-    }
+        protected String SHAEncode(String password) {
+            try {
 
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(password.getBytes("UTF-8"));
+                StringBuffer hexString = new StringBuffer();
+
+                for (int i = 0; i < hash.length; i++) {
+                    String hex = Integer.toHexString(0xff & hash[i]);
+                    if (hex.length() == 1) hexString.append('0');
+                    hexString.append(hex);
+                }
+
+                return hexString.toString();
+
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
 
 }
